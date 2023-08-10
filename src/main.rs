5f822a6faa4a45c516f6db7f9468adc77e6358bc
kaoru <k@warpnine.io>
2023-08-11 01:50:35 +0900

    fix: disable premature optimization
    For unknown reason, with the PRAGMAs enabled, only 661 repos were stored, instead of 736.

--- src/main.rs
+++ src/main.rs
@@ -133,15 +133,5 @@ fn prepare_database_connection(pool: &Pool<SqliteConnectionManager>) -> Result<(
         [],
     )?;
 
-    conn.execute_batch(
-        r#"
-PRAGMA journal_mode = OFF;
-PRAGMA synchronous = 0;
-PRAGMA cache_size = 1000000;
-PRAGMA locking_mode = EXCLUSIVE;
-PRAGMA temp_store = MEMORY;
-    "#,
-    )?;
-
     Ok(())
 }
